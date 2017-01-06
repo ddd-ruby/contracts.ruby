ca127faa004694cdd4197a836a2a5ca8424acd9c
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-06 15:00:08 +0100

    remove workaround for ruby 1.9 (since we dropped it)

--- lib/contracts/core.rb
+++ lib/contracts/core.rb
@@ -19,21 +19,6 @@ module Contracts
         end
       end
 
-      # NOTE: Workaround for `defined?(super)` bug in ruby 1.9.2
-      # source: http://stackoverflow.com/a/11181685
-      # bug: https://bugs.ruby-lang.org/issues/6644
-      base.class_eval <<-RUBY
-        # TODO: deprecate
-        # Required when contracts are included in global scope
-        def Contract(*args)
-          if defined?(super)
-            super
-          else
-            self.class.Contract(*args)
-          end
-        end
-      RUBY
-
       base.class_eval do
         def functype(funcname)
           contracts = Engine.fetch_from(self.class).decorated_methods_for(:instance_methods, funcname)
