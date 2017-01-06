922b3fe684dd91d1d736bd1466422582cf2aa338
Roman Heinrich <roman.heinrich@gmail.com>
2017-01-06 13:28:50 +0100

    move static methods up in Contract class

--- lib/contracts/contract.rb
+++ lib/contracts/contract.rb
@@ -24,67 +24,6 @@ class Contract < Contracts::Decorator
     raise data[:contracts].failure_exception.new(failure_msg(data), data)
   end
 
-  attr_reader :args_contracts, :ret_contract, :klass, :method
-  def initialize(klass, method, *contracts)
-    unless contracts.last.is_a?(Hash)
-      unless contracts.one?
-        raise %{
-          It looks like your contract for #{method.name} doesn't have a return
-          value. A contract should be written as `Contract arg1, arg2 =>
-          return_value`.
-        }.strip
-      end
-      contracts = [nil => contracts[-1]]
-    end
-
-    # internally we just convert that return value syntax back to an array
-    @args_contracts = contracts[0, contracts.size - 1] + contracts[-1].keys
-    @ret_contract   = contracts[-1].values[0]
-
-    @args_validators = args_contracts.map do |contract|
-      Contract.make_validator(contract)
-    end
-
-    @args_contract_index = args_contracts.index do |contract|
-      contract.is_a?(Contracts::Args)
-    end
-
-    @ret_validator = Contract.make_validator(ret_contract)
-    @pattern_match = false
-
-    # == @has_proc_contract
-    last_contract      = args_contracts.last
-    is_a_proc          = last_contract.is_a?(Class) && (last_contract <= Proc || last_contract <= Method)
-    maybe_a_proc       = last_contract.is_a?(Contracts::Maybe) && last_contract.include_proc?
-    @has_proc_contract = is_a_proc || maybe_a_proc || last_contract.is_a?(Contracts::Func)
-
-    # ====
-
-    # == @has_options_contract
-    last_contract         = args_contracts.last
-    penultimate_contract  = args_contracts[-2]
-    @has_options_contract = if @has_proc_contract
-                              penultimate_contract.is_a?(Hash) || penultimate_contract.is_a?(Contracts::Builtin::KeywordArgs)
-                            else
-                              last_contract.is_a?(Hash) || last_contract.is_a?(Contracts::Builtin::KeywordArgs)
-                            end
-    # ===
-
-    @klass  = klass
-    @method = method
-  end
-
-  def pretty_contract(c)
-    return c.name if c.is_a?(Class)
-    c.class.name
-  end
-
-  def to_s
-    args = args_contracts.map { |c| pretty_contract(c) }.join(", ")
-    ret  = pretty_contract(ret_contract)
-    "#{args} => #{ret}".gsub("Contracts::Builtin::", "")
-  end
-
   # Given a hash, prints out a failure message.
   # This function is used by the default #failure_callback method
   # and uses the hash passed into the failure_callback method.
@@ -146,6 +85,67 @@ class Contract < Contracts::Decorator
   # contains some useful information about the failure.
   def self.valid?(arg, contract)
     make_validator(contract)[arg]
+  end
+
+  attr_reader :args_contracts, :ret_contract, :klass, :method
+  def initialize(klass, method, *contracts)
+    unless contracts.last.is_a?(Hash)
+      unless contracts.one?
+        raise %{
+          It looks like your contract for #{method.name} doesn't have a return
+          value. A contract should be written as `Contract arg1, arg2 =>
+          return_value`.
+        }.strip
+      end
+      contracts = [nil => contracts[-1]]
+    end
+
+    # internally we just convert that return value syntax back to an array
+    @args_contracts = contracts[0, contracts.size - 1] + contracts[-1].keys
+    @ret_contract   = contracts[-1].values[0]
+
+    @args_validators = args_contracts.map do |contract|
+      Contract.make_validator(contract)
+    end
+
+    @args_contract_index = args_contracts.index do |contract|
+      contract.is_a?(Contracts::Args)
+    end
+
+    @ret_validator = Contract.make_validator(ret_contract)
+    @pattern_match = false
+
+    # == @has_proc_contract
+    last_contract      = args_contracts.last
+    is_a_proc          = last_contract.is_a?(Class) && (last_contract <= Proc || last_contract <= Method)
+    maybe_a_proc       = last_contract.is_a?(Contracts::Maybe) && last_contract.include_proc?
+    @has_proc_contract = is_a_proc || maybe_a_proc || last_contract.is_a?(Contracts::Func)
+
+    # ====
+
+    # == @has_options_contract
+    last_contract         = args_contracts.last
+    penultimate_contract  = args_contracts[-2]
+    @has_options_contract = if @has_proc_contract
+                              penultimate_contract.is_a?(Hash) || penultimate_contract.is_a?(Contracts::Builtin::KeywordArgs)
+                            else
+                              last_contract.is_a?(Hash) || last_contract.is_a?(Contracts::Builtin::KeywordArgs)
+                            end
+    # ===
+
+    @klass  = klass
+    @method = method
+  end
+
+  def pretty_contract(c)
+    return c.name if c.is_a?(Class)
+    c.class.name
+  end
+
+  def to_s
+    args = args_contracts.map { |c| pretty_contract(c) }.join(", ")
+    ret  = pretty_contract(ret_contract)
+    "#{args} => #{ret}".gsub("Contracts::Builtin::", "")
   end
 
   def [](*args, &blk)
